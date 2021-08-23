9ba58b72c47bc6b8ee8ffb33dd033ca113ed32ad
lxvs <jn.apsd@gmail.com>
2021-08-23 20:25:14 +0800

    Breaks some lines

--- lab-borrowings.vb
+++ lab-borrowings.vb
@@ -14,18 +14,32 @@ Private Sub Worksheet_Change(ByVal Target As Range)
 
     col_num_status = 5
     col_num_history = 7
-    col_status = Choose(col_num_status, "A", "B", "C", "D", "E", "F", "G", "H", "I")
-    col_history = Choose(col_num_history, "A", "B", "C", "D", "E", "F", "G", "H", "I")
+    col_status = Choose(col_num_status, _
+        "A", "B", "C", "D", "E", "F", "G", "H", "I")
+    col_history = Choose(col_num_history, _
+        "A", "B", "C", "D", "E", "F", "G", "H", "I")
 
-    If Target.Column = col_num_status And Sheet1.Range(col_status & Target.Row) <> "" And Sheet1.Range("A" & Target.Row) <> "" Then
+    If Target.Column = col_num_status _
+        And Sheet1.Range(col_status & Target.Row) <> "" _
+        And Sheet1.Range("A" & Target.Row) <> "" Then
         If Sheet1.Range(col_history & Target.Row) = "" Then
-            Sheet1.Range(col_history & Target.Row) = Format(Now, "yyyy-MM-dd") & " " & Target
+            Sheet1.Range(col_history & Target.Row) _
+                = Format(Now, "yyyy-MM-dd") & " " & Target
         ElseIf InStr(Sheet1.Range(col_history & Target.Row), "|") = 0 Then
-            If Sheet1.Range(col_history & Target.Row) <> Format(Now, "yyyy-MM-dd") & " " & Target Then Sheet1.Range(col_history & Target.Row) = Format(Now, "yyyy-MM-dd") & " " & Target & "  |  " & Sheet1.Range(col_history & Target.Row)
+            If Sheet1.Range(col_history & Target.Row) _
+                <> Format(Now, "yyyy-MM-dd") & " " & Target Then
+                Sheet1.Range(col_history & Target.Row) _
+                    = Format(Now, "yyyy-MM-dd") & " " & Target _
+                    & "  |  " & Sheet1.Range(col_history & Target.Row)
+            End If
         Else
             Dim logs() As String
             logs = split(Sheet1.Range(col_history & Target.Row), "  |  ")
-            If logs(0) <> Format(Now, "yyyy-MM-dd") & " " & Target Then Sheet1.Range(col_history & Target.Row) = Format(Now, "yyyy-MM-dd") & " " & Target & "  |  " & Sheet1.Range(col_history & Target.Row)
+            If logs(0) <> Format(Now, "yyyy-MM-dd") & " " & Target Then
+                Sheet1.Range(col_history & Target.Row) _
+                    = Format(Now, "yyyy-MM-dd") & " " & Target _
+                    & "  |  " & Sheet1.Range(col_history & Target.Row)
+            End If
         End If
     End If
 End Sub
