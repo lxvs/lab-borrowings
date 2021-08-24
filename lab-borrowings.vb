babeea406ccb307f1498ff77befb3874b067c090
lxvs <jn.apsd@gmail.com>
2021-08-24 09:06:32 +0800

    Add a variable 'date_now'

--- lab-borrowings.vb
+++ lab-borrowings.vb
@@ -12,12 +12,15 @@ Private Sub Worksheet_Change(ByVal Target As Range)
     Dim col_status As String
     Dim col_history As String
 
+    Dim date_now As String
+
     col_num_status = 5
     col_num_history = 7
     col_status = Choose(col_num_status, _
         "A", "B", "C", "D", "E", "F", "G", "H", "I")
     col_history = Choose(col_num_history, _
         "A", "B", "C", "D", "E", "F", "G", "H", "I")
+    date_now = Format(Now, "yyyy-MM-dd")
 
     If Target.Column <> col_num_status _
         Or Sheet1.Range(col_status & Target.Row) = "" _
@@ -27,24 +30,24 @@ Private Sub Worksheet_Change(ByVal Target As Range)
 
     If Sheet1.Range(col_history & Target.Row) = "" Then
         Sheet1.Range(col_history & Target.Row) _
-            = Format(Now, "yyyy-MM-dd") & " " & Target
+            = date_now & " " & Target
         Exit Sub
     End If
 
     If InStr(Sheet1.Range(col_history & Target.Row), "|") = 0 _
         And Sheet1.Range(col_history & Target.Row) _
-        <> Format(Now, "yyyy-MM-dd") & " " & Target Then
+        <> date_now & " " & Target Then
         Sheet1.Range(col_history & Target.Row) _
-            = Format(Now, "yyyy-MM-dd") & " " & Target _
+            = date_now & " " & Target _
             & "  |  " & Sheet1.Range(col_history & Target.Row)
         Exit Sub
     End If
 
     Dim logs() As String
     logs = split(Sheet1.Range(col_history & Target.Row), "  |  ")
-    If logs(0) <> Format(Now, "yyyy-MM-dd") & " " & Target Then
+    If logs(0) <> date_now & " " & Target Then
         Sheet1.Range(col_history & Target.Row) _
-            = Format(Now, "yyyy-MM-dd") & " " & Target _
+            = date_now & " " & Target _
             & "  |  " & Sheet1.Range(col_history & Target.Row)
         Exit Sub
     End If
