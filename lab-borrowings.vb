9d73bf63aeb0c3a54ee25d87240b750a37d66f05
lxvs <jn.apsd@gmail.com>
2022-03-28 11:03:15 +0800

    Now can handle multi-cell changes

--- lab-borrowings.vb
+++ lab-borrowings.vb
@@ -8,11 +8,11 @@ Private Sub Worksheet_Change(ByVal Target As Range)
 
     Dim col_num_status As Integer
     Dim col_num_history As Integer
-
     Dim col_status As String
     Dim col_history As String
-
     Dim date_now As String
+    Dim r As Integer
+    Dim i As Integer
 
     col_num_status = 5
     col_num_history = 7
@@ -20,34 +20,39 @@ Private Sub Worksheet_Change(ByVal Target As Range)
         "A", "B", "C", "D", "E", "F", "G", "H", "I")
     col_history = Choose(col_num_history, _
         "A", "B", "C", "D", "E", "F", "G", "H", "I")
-    date_now = Format(Now, "yyyy-MM-dd")
 
-    If Target.Column <> col_num_status _
-        Or Sheet1.Range(col_status & Target.Row) = "" _
-        Or Sheet1.Range("A" & Target.Row) = "" Then
+    If Intersect(Target, Sheet1.Range(col_status & ":" & col_status)) _
+        Is Nothing Then
         Exit Sub
     End If
 
-    If Sheet1.Range(col_history & Target.Row) = "" Then
-        Sheet1.Range(col_history & Target.Row) = date_now & " " & Target
-        Exit Sub
-    End If
-
-    If InStr(Sheet1.Range(col_history & Target.Row), vbLf) = 0 _
-        And Sheet1.Range(col_history & Target.Row) _
-        <> date_now & " " & Target Then
-        Sheet1.Range(col_history & Target.Row) _
-            = date_now & " " & Target _
-            & vbLf & Sheet1.Range(col_history & Target.Row)
-        Exit Sub
-    End If
-
-    Dim logs() As String
-    logs = split(Sheet1.Range(col_history & Target.Row), vbLf)
-    If logs(0) <> date_now & " " & Target Then
-        Sheet1.Range(col_history & Target.Row) _
-            = date_now & " " & Target _
-            & vbLf & Sheet1.Range(col_history & Target.Row)
-        Exit Sub
-    End If
+    For i = 0 To Target.Rows.Count - 1
+        r = Target.Row + i
+        If Sheet1.Range(col_status & r) = "" _
+            Or Sheet1.Range("A" & r) = "" Then
+            GoTo continue
+        End If
+        date_now = Format(Now, "yyyy-MM-dd")
+        If Sheet1.Range(col_history & r) = "" Then
+            Sheet1.Range(col_history & r) = _
+                date_now & " " & Sheet1.Range(col_status & r)
+            GoTo continue
+        End If
+        If InStr(Sheet1.Range(col_history & r), vbLf) = 0 _
+            And Sheet1.Range(col_history & r) _
+            <> date_now & " " & Sheet1.Range(col_status & r) Then
+            Sheet1.Range(col_history & r) _
+                = date_now & " " & Sheet1.Range(col_status & r) _
+                & vbLf & Sheet1.Range(col_history & r)
+            GoTo continue
+        End If
+        Dim logs() As String
+        logs = Split(Sheet1.Range(col_history & r), vbLf)
+        If logs(0) <> date_now & " " & Sheet1.Range(col_status & r) Then
+            Sheet1.Range(col_history & r) _
+                = date_now & " " & Sheet1.Range(col_status & r) _
+                & vbLf & Sheet1.Range(col_history & r)
+        End If
+continue:
+    Next i
 End Sub
