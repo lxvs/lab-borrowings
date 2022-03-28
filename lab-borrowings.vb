5ffb1e6b2e77f1edd424154fa3a296fbbd7b0b25
lxvs <jn.apsd@gmail.com>
2022-03-28 11:34:35 +0800

    Set worksheet as a variable

--- lab-borrowings.vb
+++ lab-borrowings.vb
@@ -6,6 +6,7 @@ Option Explicit
 
 Private Sub Worksheet_Change(ByVal Target As Range)
 
+    Dim sheet As Worksheet
     Dim col_num_status As Integer
     Dim col_num_history As Integer
     Dim col_status As String
@@ -14,6 +15,7 @@ Private Sub Worksheet_Change(ByVal Target As Range)
     Dim r As Integer
     Dim i As Integer
 
+    Set sheet = Sheet1
     col_num_status = 5
     col_num_history = 7
     col_status = Choose(col_num_status, _
@@ -21,29 +23,29 @@ Private Sub Worksheet_Change(ByVal Target As Range)
     col_history = Choose(col_num_history, _
         "A", "B", "C", "D", "E", "F", "G", "H", "I")
 
-    If Intersect(Target, Sheet1.Range(col_status & ":" & col_status)) _
+    If Intersect(Target, sheet.Range(col_status & ":" & col_status)) _
         Is Nothing Then
         Exit Sub
     End If
 
     For i = 0 To Target.Rows.Count - 1
         r = Target.Row + i
-        If Sheet1.Range(col_status & r) = "" _
-            Or Sheet1.Range("A" & r) = "" Then
+        If sheet.Range(col_status & r) = "" _
+            Or sheet.Range("A" & r) = "" Then
             GoTo continue
         End If
         date_now = Format(Now, "yyyy-MM-dd")
-        If Sheet1.Range(col_history & r) = "" Then
-            Sheet1.Range(col_history & r) = _
-                date_now & " " & Sheet1.Range(col_status & r)
+        If sheet.Range(col_history & r) = "" Then
+            sheet.Range(col_history & r) = _
+                date_now & " " & sheet.Range(col_status & r)
             GoTo continue
         End If
         Dim logs() As String
-        logs = Split(Sheet1.Range(col_history & r), vbLf)
-        If logs(0) <> date_now & " " & Sheet1.Range(col_status & r) Then
-            Sheet1.Range(col_history & r) _
-                = date_now & " " & Sheet1.Range(col_status & r) _
-                & vbLf & Sheet1.Range(col_history & r)
+        logs = Split(sheet.Range(col_history & r), vbLf)
+        If logs(0) <> date_now & " " & sheet.Range(col_status & r) Then
+            sheet.Range(col_history & r) _
+                = date_now & " " & sheet.Range(col_status & r) _
+                & vbLf & sheet.Range(col_history & r)
         End If
 continue:
     Next i
